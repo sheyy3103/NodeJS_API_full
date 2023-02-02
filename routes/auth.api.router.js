385eb6483f16791c8f3949e5157a4f99a688bd48
Sheyy <kelvinhuynhalves1102@gmail.com>
2023-02-02 13:35:03 +0700

    any little change

--- routes/auth.api.router.js
+++ routes/auth.api.router.js
@@ -2,9 +2,7 @@ const userController = require('../controllers/userController');
 
 module.exports = (app) => {
     app.get('/api/account', userController.getAll);
-
     app.post('/api/register', userController.register);
     app.post('/api/login', userController.login);
-
     app.post('/api/token', userController.refreshToken);
 };
